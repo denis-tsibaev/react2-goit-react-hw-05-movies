47918aba2279b13630ce3daeba2587be94bc12c0
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-11-23 23:51:36 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import HomePage from './components/HomePage';
+import MoviesPage from './components/MoviesPage';
+
 function App() {
-    return <div className="App">444555</div>;
+    return (
+        <div className="App">
+            <HomePage />
+            <MoviesPage />
+        </div>
+    );
 }
 
 export default App;
